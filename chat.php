d37ef14e9c37f1dc85a19ee3f5a83001f680a51e
jyoti-wp <sim_s006@icloud.com>
2021-06-14 15:48:39 +0530

    stylesheet update

---
+++ chat.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <title>Real Time Chat App</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <a href="" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="images/image.jpeg" alt="">
+                <div class="details">
+                    <span>Jyoti Shukla</span>
+                    <p>Active now</p>
+                </div>
+            </header>
+            <div class="chat-box">
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, non ea odit repellendus quasi.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <!-- <img src="images/image.jpeg" alt=""> -->
+                    <div class="details">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, non ea odit repellendus quasi.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
